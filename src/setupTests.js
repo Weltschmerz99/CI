3f5d6a046a705b1579ffa7a38b6d5cfbedf05dc7
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-04-05 18:31:18 +0500

    add failing test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+test('Тест, который всегда упадет', () => {
+  expect(1 + 1).toBe(3);
+});
